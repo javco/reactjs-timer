85247ea9a62e38092de7b92c1a152059c4ebf258
javco <jvrcastilla@gmail.com>
2018-03-14 12:05:27 +0100

    some texts changed and css added

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # reactjs-timer
+
+Simple chronometer to obtain total amount of time between on/off timer status
+
+## Version
+1.0.0
--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React Timer</h1>
         </header>
         <p className="App-intro">
           Click the button to start/stop the timer
